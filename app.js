a7aa5e2d04fb06373e70dbf9c55a3a0386cf0c74
DivyanKavdia <divyan.dk@gmail.com>
2026-09-18 15:13:06 +0530

    Ship shell146 for Bluefy ownership recovery

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
   const APP_VERSION = "1.0.0";
   const APP_REVISION = "1.0.0-audio6";
-  const APP_SHELL_REVISION = "1.0.0-shell145-chakshu-voice";
+  const APP_SHELL_REVISION = "1.0.0-shell146-bluefy-ownership";
   let deviceAssociation = null;
   let deviceIdentityMessage = "Not connected";
   const PROTOCOL_VERSION = 0x02;
